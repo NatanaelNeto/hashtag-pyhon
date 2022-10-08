8ea8ee3ee2d7363f31b515eff71e20ac33ecb3b3
Natanael Neto <natanael.eneas2018@hotmail.com>
2022-10-08 05:30:11 -0300

    limpa código e comenta

--- main.py
+++ main.py
@@ -1,57 +1,51 @@
-from locale import normalize
 import pandas as pd
 import plotly.express as px
 
+# Importa a tabela
 tabela = pd.read_csv("telecom_users.csv")
 
+# Ajusta os dados
 tabela = tabela.drop("Unnamed: 0", axis=1)
-
 tabela["TotalGasto"] = pd.to_numeric(tabela["TotalGasto"], errors="coerce")
-
 tabela = tabela.dropna(how="all", axis=1)
 tabela = tabela.dropna(how="any", axis=0)
 
-# Verifica, considerando os aposentados, qual o maior índice de churn
-lookup1 = (
-    tabela.loc[:, ["Churn", "Casado"]].value_counts(normalize=True).loc[["Sim"], :]
-)
+#  Teste de conceito
+teste = tabela.loc[:, ["Churn", "Casado"]].value_counts(normalize=True).loc[["Sim"], :]
+teste_dict = teste.to_dict()
 
-lookup1_dict = lookup1.to_dict()
+# Reserva valor de Churn total
+# churn = tabela["Churn"].value_counts()["Sim"]
 
-# lookup1_normalize = lookup1.loc[["Sim"] == lookup1.max().item(), :]
-
-# lookup1_percent = "{:.1%}".format(lookup1.max())
-
-# print(lookup1_dict)
-
-
-# Total de Churn na base de dados
-churn = tabela["Churn"].value_counts()["Sim"]
-
-# Verifica quais as maiores porcentagens de inferência em cada coluna, comparado com o Churn
-
+# Inicia dict para receber os dados
 lookup_dict = {"Dado": [], "Churn": []}
 
+# Para cada dado na tabela, retorna o maior influenciador no churn
 for tag in tabela.columns:
+    # Ignora colunas desnecessárias
     if tag != "Churn" and tag != "IDCliente":
+
+        # Para cada dado, cria um pandas.Series que agrupa os dados com base no Churn
         aux_lookup = (
             tabela.loc[:, ["Churn", tag]].value_counts(normalize=True).loc[["Sim"], :]
         )
+
+        # Converte os dados para dict e recupera os valores
         aux_dict = aux_lookup.to_dict()
         all_values = aux_dict.values()
         max_value = "{:.4}".format(max(all_values))
         max_key = max(aux_dict, key=aux_dict.get)
-        # print(tag, " -> ", max_key[1], " -> ", max_value)
 
         lookup_dict["Dado"].append("{tag}: {key}".format(tag=tag, key=max_key[1]))
         lookup_dict["Churn"].append(float(max_value) * 100)
 
-# print(lookup_dict)
-
+# Retorna a forma de pandas.DataFrame
 analise = pd.DataFrame.from_dict(lookup_dict)
 
+# Teste
 print(analise)
 
+# Plot do gráfico
 grafico = px.histogram(
     analise,
     x="Dado",
